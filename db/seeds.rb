95ecd0d0f4894ede450a98292d4701a0b03b3b08
gregoryawesoman <ray_couper@mail.ru>
2020-04-02 00:28:46 +0300

    feed db with some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([
+                  { title: 'Ruby' },
+                  { title: 'DevOps' },
+                  { title: 'HTML' }
+                ])
+Test.create([
+              { title: 'Ruby basics', level: 1, category_id: 1 },
+              { title: 'Ruby improved', level: 2, category_id: 1 },
+              { title: 'CI/CD', level: 2, category_id: 2 },
+              { title: 'Containerization software', level: 3, category_id: 2 },
+              { title: 'HTML/CSS', level: 1, category_id: 3 }
+            ])
+Question.create([
+                  { body: 'How to include module in class?', test_id: 1 },
+                  { body: 'How to forward ports from docker containers?', test_id: 4 },
+                  { body: 'Test question1', test_id: 2 },
+                  { body: 'Another test question', test_id: 3 }
+                ])
+Answer.create([
+                { body: 'Some data1', correct: true, question_id: 1 },
+                { body: 'Some data2', correct: false, question_id: 1 },
+                { body: 'Some data3', correct: true, question_id: 2 },
+                { body: 'Some data4', correct: false, question_id: 2 },
+                { body: 'Some data5', correct: false, question_id: 2 }
+              ])
+User.create([
+              { name: 'admin', email: 'admin@example.com' },
+              { name: 'customer', email: 'customer@example.com' }
+            ])
