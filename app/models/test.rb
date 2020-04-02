c099dad83cca70eed7f89c4a8aaab245d0a432e9
gregoryawesoman <ray_couper@mail.ru>
2020-04-03 01:29:32 +0300

    users tests by level

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_and_belongs_to_many :users
 
   def self.by_category(name)
     Test.select(:title).joins(:category).where('categories.title': name).order(id: :desc)
