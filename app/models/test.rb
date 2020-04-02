cbbb85971da80421ecc50b56cd5c84bf393447f7
gregoryawesoman <ray_couper@mail.ru>
2020-04-02 19:49:13 +0300

    test by category_name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.by_category(name)
+    Test.select(:title).joins(:category).where('categories.title': name).order(id: :desc)
+  end
 end
