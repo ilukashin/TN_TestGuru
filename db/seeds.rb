f83725cb81ed2b2504392863735f20214039b9b0
gregoryawesoman <ray_couper@mail.ru>
2020-04-03 18:12:39 +0300

    seeds fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,40 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-TESTS_USERS_RELATIONS = 'INSERT into tests_users VALUES
-  (1, 1),
-  (3, 1),
-  (5, 1),
-  (2, 2),
-  (4, 2);'
-
-Category.create([
+categories = Category.create!([
                   { title: 'Ruby' },
                   { title: 'DevOps' },
                   { title: 'HTML' }
                 ])
-Test.create([
-              { title: 'Ruby basics', level: 1, category_id: 1 },
-              { title: 'Ruby improved', level: 2, category_id: 1 },
-              { title: 'CI/CD', level: 2, category_id: 2 },
-              { title: 'Containerization software', level: 3, category_id: 2 },
-              { title: 'HTML/CSS', level: 1, category_id: 3 }
+
+tests = Test.create!([
+              { title: 'Ruby basics', level: 1, category_id: categories[0].id },
+              { title: 'Ruby improved', level: 2, category_id: categories[0].id },
+              { title: 'CI/CD', level: 2, category_id: categories[1].id },
+              { title: 'Containerization software', level: 3, category_id: categories[1].id },
+              { title: 'HTML/CSS', level: 1, category_id: categories[2].id }
             ])
-Question.create([
-                  { body: 'How to include module in class?', test_id: 1 },
-                  { body: 'How to forward ports from docker containers?', test_id: 4 },
-                  { body: 'Test question1', test_id: 2 },
-                  { body: 'Another test question', test_id: 3 }
+
+questions = Question.create!([
+                  { body: 'How to include module in class?', test_id: tests[0].id },
+                  { body: 'How to forward ports from docker containers?', test_id: tests[3].id },
+                  { body: 'Test question1', test_id: tests[1].id },
+                  { body: 'Another test question', test_id: tests[2].id }
                 ])
-Answer.create([
-                { body: 'Some data1', correct: true, question_id: 1 },
-                { body: 'Some data2', correct: false, question_id: 1 },
-                { body: 'Some data3', correct: true, question_id: 2 },
-                { body: 'Some data4', correct: false, question_id: 2 },
-                { body: 'Some data5', correct: false, question_id: 2 }
+
+Answer.create!([
+                { body: 'Some data1', correct: true, question_id: questions[0].id },
+                { body: 'Some data2', correct: false, question_id: questions[0].id },
+                { body: 'Some data3', correct: true, question_id: questions[1].id },
+                { body: 'Some data4', correct: false, question_id: questions[2].id },
+                { body: 'Some data5', correct: false, question_id: questions[2].id }
               ])
-User.create([
+
+users = User.create!([
               { name: 'admin', email: 'admin@example.com' },
               { name: 'customer', email: 'customer@example.com' }
             ])
+
+TESTS_USERS_RELATIONS = "INSERT into tests_users VALUES
+  (#{tests[0].id}, #{users[0].id}),
+  (#{tests[2].id}, #{users[0].id}),
+  (#{tests[4].id}, #{users[0].id}),
+  (#{tests[1].id}, #{users[1].id}),
+  (#{tests[3].id}, #{users[1].id});"
 ActiveRecord::Base.connection.execute(TESTS_USERS_RELATIONS)
