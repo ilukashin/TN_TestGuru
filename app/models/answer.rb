c47e2f9748a7ee1652bbd8efcee7adb070d0f547
gregoryawesoman <ray_couper@mail.ru>
2020-04-09 10:11:32 +0300

    added validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,19 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  validates :body, presence: true
 
   scope :correct, -> { where(correct: true) }
+
+  validate :max_answers
+  validate :min_answers
+
+  private
+
+  def max_answers
+    errors.add(question, 'Answers limit reached!') if question.answers.count >= 4
+  end
+
+  def min_answers
+    errors.add(question, 'Answers minimum reached!') if question.answers.count <= 1
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than: -1 }
+
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
