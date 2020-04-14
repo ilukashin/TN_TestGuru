f725a0100153861c0684733ed8bea9145a0ba9b9
gregoryawesoman <ray_couper@mail.ru>
2020-04-14 10:28:07 +0300

    read option for tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,14 @@
 class TestsController < ApplicationController
-  before_action :find_question
+  before_action :find_question, only: %i[show]
 
   def show 
     render 'show'
   end
 
+  def index
+    @tests = Test.all
+  end
+    
   private
 
   def find_question
