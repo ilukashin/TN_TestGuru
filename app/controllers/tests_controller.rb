a68a21c4acabd77c43a1d355ca258e3f56719567
gregoryawesoman <ray_couper@mail.ru>
2020-04-18 20:13:32 +0300

    added test passage logic & views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,6 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :set_user, only: :start
 
   def show; end
 
@@ -34,6 +35,11 @@ class TestsController < ApplicationController
     @test.destroy
     redirect_to tests_path
   end
+
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
     
   private
 
@@ -43,5 +49,9 @@ class TestsController < ApplicationController
 
   def post_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def set_user
+    @user = User.first
   end
 end
---
+++ app/models/test_passage.rb
@@ -0,0 +1,52 @@
+class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validatation_set_next_question
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+    
+    save!
+  end
+
+  def success?
+    percent_correct >= 85
+  end
+
+  def percent_correct
+    correct_questions.to_f / test.questions.count * 100
+  end
+
+  private 
+
+  def before_validatation_set_next_question
+    self.current_question = next_question
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    if current_question.nil?
+      test_questions.first
+    else
+      test_questions.where('id > ?', current_question.id).first
+    end
+  end
+
+  def test_questions
+    test.questions.order(:id)
+  end
+
+end
