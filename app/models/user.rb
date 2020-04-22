202ff97d28439357f7d97dda742d41edb3c161e9
gregoryawesoman <ray_couper@mail.ru>
2020-04-22 20:45:57 +0300

    email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   has_secure_password
 
