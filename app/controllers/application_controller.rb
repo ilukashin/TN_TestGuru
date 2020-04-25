918564a0194d4bdde8b137defe8c2007cbc9bdc7
gregoryawesoman <ray_couper@mail.ru>
2020-04-25 23:01:59 +0300

    added devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user
-  helper_method :logged_in?
+  protected
 
-  private 
-
-  def authenticate_user!
-    unless current_user
-      session[:request_url] = request.url
-      redirect_to login_path, alert: 'You are not authorized to view this content'
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
