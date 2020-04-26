9896b947c9267c169e817f3261824c4231bc4ad7
gregoryawesoman <ray_couper@mail.ru>
2020-04-26 12:56:19 +0300

    fixed missed dependencies

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions, dependent: :destroy
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
 
   validates :title, presence: true,
