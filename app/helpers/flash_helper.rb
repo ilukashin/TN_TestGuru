7aa00c0cc85143503ca63056e986eaded9313ce6
gregoryawesoman <ray_couper@mail.ru>
2020-04-30 21:04:56 +0300

    flash classes fix

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,4 +2,12 @@ module FlashHelper
   def flash_message(type)
     content_tag :p, flash[type], class: "flash #{type}" if flash[type] 
   end
+
+  def bootsrap_type(type)
+    case type
+    when :notice then 'info'
+    when :alert then 'warning'
+    when :error then 'danger'
+    end
+  end
 end
