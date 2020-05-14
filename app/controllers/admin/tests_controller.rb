58b1d864c9f0b2438762ca893512ee3de8071ff3
gregoryawesoman <ray_couper@mail.ru>
2020-05-15 01:12:40 +0300

    added inline update for tests, with fix in js

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,19 +1,18 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   def show; end
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def new
     @test = Test.new
   end
 
   def create
-    @test = Test.new(post_params)
+    @test = Test.new(test_params)
     @test.author_id = current_user.id
 
     if @test.save
@@ -26,10 +25,18 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def update
-    if @test.update(post_params)
+    if @test.update(test_params)
       redirect_to admin_tests_path
     else
-      render 'edit'
+      render :edit
+    end
+  end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
     end
   end
 
@@ -40,11 +47,15 @@ class Admin::TestsController < Admin::BaseController
     
   private
 
+  def find_tests
+    @tests = Test.all
+  end
+
   def find_test
     @test = Test.find(params[:id])
   end
 
-  def post_params
+  def test_params
     params.require(:test).permit(:title, :level, :category_id)
   end
 end
