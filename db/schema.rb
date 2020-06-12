061bef3f2b8eda4775e7ba8bfd965f5d79e4e92b
gregoryawesoman <ray_couper@mail.ru>
2020-06-12 21:37:43 +0300

    added value for badges rule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_125749) do
+ActiveRecord::Schema.define(version: 2020_06_12_171021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_125749) do
     t.string "rule", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "value", null: false
   end
 
   create_table "badges_users", id: false, force: :cascade do |t|
